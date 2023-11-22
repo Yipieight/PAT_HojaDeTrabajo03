95e99294e8012ae1acaa402c9ed4743c22da4674
Yipieight <57154305+Yipieight@users.noreply.github.com>
2023-11-22 13:22:53 -0600

    All exercise resolve

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -1,6 +1,30 @@
 #include "Ejercicio02.h"
+#include <string>
+#include <vector>
+#include <algorithm>
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    return nullptr;
+    vector<string> sortedStrings;
+    vector<vector<string>> anagramGroups;
+
+    for (const string& s : strings) {
+        string sortedStr = s;
+        sort(sortedStr.begin(), sortedStr.end());
+
+        auto it = find(sortedStrings.begin(), sortedStrings.end(), sortedStr);
+
+        if (it != sortedStrings.end()) {
+            size_t index = distance(sortedStrings.begin(), it);
+            anagramGroups[index].push_back(s);
+        }
+        else {
+            sortedStrings.push_back(sortedStr);
+            anagramGroups.push_back({ s });
+        }
+    }
+
+    auto result = new vector<vector<string>>(anagramGroups);
+
+    return result;
 }
